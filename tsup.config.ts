37196dde232c625f2542afc86e8e596ad2e6f904
Danil Trapeznikov <danil@iglu.net>
2026-01-19 15:27:34 +0700

    refactor(tsup.config.ts): simplify configuration by merging entry points and removing redundant settings for better maintainability and clarity

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,27 +1,20 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig([
-  {
-    entry: {
-      index: 'src/index.ts',
-    },
-    format: ['cjs', 'esm'],
-    dts: true,
-    splitting: false,
-    sourcemap: true,
-    clean: true,
+export default defineConfig({
+  entry: {
+    index: 'src/index.ts',
+    generator: 'src/generator.ts',
   },
-  {
-    entry: {
-      generator: 'src/generator.ts',
-    },
-    format: ['cjs'],
-    dts: false,
-    splitting: false,
-    sourcemap: false,
-    clean: false,
-    banner: {
-      js: '#!/usr/bin/env node',
-    },
+  format: ['cjs', 'esm'],
+  dts: true,
+  clean: true,
+  sourcemap: true,
+  splitting: false,
+  treeshake: true,
+  minify: false,
+  outExtension({ format }) {
+    return {
+      js: format === 'cjs' ? '.cjs' : '.js',
+    }
   },
-])
+})
