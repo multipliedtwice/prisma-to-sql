cc4a49ef8573cc946e9f10727add25d8b5b64388
Danil Trapeznikov <danil@iglu.net>
2026-02-05 00:23:13 +0700

    feat: rerun benchmarks

--- postgres-v7.config.ts
+++ postgres-v7.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
-  schema: 'tests/prisma/schema-v7.prisma',
+  schema: 'tests/prisma/schema-postgres-v7.prisma',
   datasource: {
     url: 'postgres://postgres:postgres@localhost:5433/prisma_test',
   },
